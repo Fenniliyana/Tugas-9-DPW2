58e0c75c9645af830510c6bed6831a37422a783c
Fenniliyana <91305370+Fenniliyana@users.noreply.github.com>
2021-11-25 11:54:37 +0700

    Tugas 9 DPW2

---
+++ system/app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php  
+namespace App\Http\Controllers;
+
+
+class HomeController extends controller{
+
+
+   function showberanda(){
+       return view('admin.beranda');
+   }
+
+   function showproduk(){
+    return view('admin.produk.index');
+   }
+
+   function showtemplate(){
+    return view('admin.template.base');
+   }
+
+   function showkategori(){
+    return view('admin.kategori.index');
+   }
+
+   function showuser(){
+    return view('admin.user.index');
+   }
+
+   function test($produk, $hargaProduk = 0){
+
+    echo "Nama Produk $produk";
+    echo "<br>";
+    echo "Harga Produk adalah $hargaProduk <br";
+   }
+}
